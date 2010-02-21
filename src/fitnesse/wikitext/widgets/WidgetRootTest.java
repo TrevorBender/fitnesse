92157f60a04f7644dda2f5c4da3cffa121634124
TrevorBender <Trevor.Bender@gmail.com>
2010-02-21 03:13:06 -0500

    Added PARENT_NAME and PARENT_PAGE variable

--- src/fitnesse/wikitext/widgets/WidgetRootTest.java
+++ src/fitnesse/wikitext/widgets/WidgetRootTest.java
@@ -2,23 +2,14 @@
 // Released under the terms of the CPL Common Public License version 1.0.
 package fitnesse.wikitext.widgets;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Before;
-import org.junit.Test;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import util.RegexTestCase;
 import fitnesse.FitNesse;
 import fitnesse.FitNesseContext;
-import fitnesse.wiki.InMemoryPage;
-import fitnesse.wiki.PageCrawler;
-import fitnesse.wiki.PageData;
-import fitnesse.wiki.PathParser;
-import fitnesse.wiki.WikiPage;
+import fitnesse.wiki.*;
+import org.junit.Before;
+import org.junit.Test;
+import util.RegexTestCase;
+
+import static org.junit.Assert.assertEquals;
 
 public class WidgetRootTest {
   private WikiPage rootPage;
@@ -39,7 +30,57 @@ public class WidgetRootTest {
   }
 
   @Test
-    public void testVariablesOneTheRootPage() throws Exception {
+  public void testParentPageNameVariable() throws Exception
+  {
+    PageCrawler crawler = rootPage.getPageCrawler();
+    crawler.setDeadEndStrategy(new VirtualEnabledPageCrawler());
+    final String ROOT_PAGE_NAME = "RootPage";
+    WikiPage root = crawler.addPage(rootPage, PathParser.parse(ROOT_PAGE_NAME));
+    final String INCLUDED_PAGE_NAME = "IncludedPage";
+    WikiPage includedPage = crawler.addPage(rootPage, PathParser.parse(INCLUDED_PAGE_NAME));
+    WidgetRoot widgetRoot = new WidgetRoot(root);
+    WidgetRoot includedRoot = new WidgetRoot(includedPage,widgetRoot);
+    PageData data = includedPage.getData();
+    assertEquals(INCLUDED_PAGE_NAME,data.getVariable("PAGE_NAME"));
+    assertEquals(INCLUDED_PAGE_NAME, includedRoot.getVariable("PAGE_NAME"));
+    assertEquals(ROOT_PAGE_NAME,includedRoot.getVariable("PARENT_NAME"));
+  }
+
+  @Test
+  public void testParentPageNameVariableWithNoParent() throws Exception
+  {
+    PageCrawler crawler = rootPage.getPageCrawler();
+    crawler.setDeadEndStrategy(new VirtualEnabledPageCrawler());
+    WikiPage root = crawler.addPage(rootPage, PathParser.parse("RootPage"));
+    WikiPage includedPage = crawler.addPage(rootPage, PathParser.parse("IncludedPage"));
+    WidgetRoot includedRoot = new WidgetRoot(includedPage);
+    assertEquals("IncludedPage", includedRoot.getVariable("PAGE_NAME"));
+    // PARENT_NAME returns PAGE_NAME if the page isn't included.
+    assertEquals("IncludedPage",includedRoot.getVariable("PARENT_NAME"));
+  }
+
+  @Test
+  public void testMultipleLevelsOfIncludedPages() throws Exception
+  {
+    PageCrawler crawler = rootPage.getPageCrawler();
+    crawler.setDeadEndStrategy(new VirtualEnabledPageCrawler());
+    final String ROOT_PAGE_NAME = "RootPage";
+    WikiPage root = crawler.addPage(rootPage, PathParser.parse(ROOT_PAGE_NAME));
+    final String INCLUDED_PAGE_NAME = "IncludedPage";
+    WikiPage includedPage = crawler.addPage(rootPage, PathParser.parse(INCLUDED_PAGE_NAME));
+    final String SECOND_LEVEL_INCLUDED_PAGE_NAME = "SecondLevelIncludedPage";
+    WikiPage secondLevelIncludedPage  = crawler.addPage(rootPage, PathParser.parse(SECOND_LEVEL_INCLUDED_PAGE_NAME));
+    WidgetRoot widgetRoot = new WidgetRoot(root);
+    WidgetRoot includedRoot = new WidgetRoot(includedPage,widgetRoot);
+    WidgetRoot secondLevelRoot = new WidgetRoot(secondLevelIncludedPage,includedRoot);
+    PageData data = secondLevelIncludedPage.getData();
+    assertEquals(SECOND_LEVEL_INCLUDED_PAGE_NAME,data.getVariable("PAGE_NAME"));
+    assertEquals(SECOND_LEVEL_INCLUDED_PAGE_NAME, secondLevelRoot.getVariable("PAGE_NAME"));
+    assertEquals(ROOT_PAGE_NAME,secondLevelRoot.getVariable("PARENT_NAME"));
+  }
+
+  @Test
+  public void testVariablesOneTheRootPage() throws Exception {
     WikiPage root = InMemoryPage.makeRoot("RooT");
     PageData data = root.getData();
     data.setContent("!define v1 {Variable #1}\n");
@@ -116,44 +157,5 @@ public class WidgetRootTest {
     PageData data = page.getData();
     String html = data.getHtml();
     assertEquals("<i>italics</i><br/><b>bold</b><br/>", html);
-  }
-
-  @Test
-  public void nestedTableExpansion () throws Exception {
-    WikiPage root = InMemoryPage.makeRoot("RooT");
-    PageData data = root.getData();
-    data.setContent("!define AA {|aa|\n}\n" +
-                    "!define BB (|${AA}|\n)\n"
-                   );
-    root.commit(data);
-    WikiPage page = root.getPageCrawler().addPage(root, PathParser.parse("SomePage"), "${BB}\n");
-    data = page.getData();
-    String html = data.getHtml();
-    
-    boolean found = Pattern
-                    .compile("<table.+<table.+</table.+</table", Pattern.DOTALL)
-                    .matcher(html).find();
-    assertTrue(html, found);
-  }
-  
-  @Test
-  public void nestedNewlineExpansion () throws Exception {
-    WikiPage root = InMemoryPage.makeRoot("RooT");
-    PageData data = root.getData();
-    data.setContent("!define LIST {\n * list\n}\n" +
-                    "!define BB (|${LIST}|\n)\n"
-                   );
-    root.commit(data);
-    WikiPage page = root.getPageCrawler().addPage(root, PathParser.parse("SomePage"), "${BB}\n");
-    data = page.getData();
-    String html = data.getHtml();
-
-    boolean found = Pattern
-                    .compile("<table.+<td.+<ul.+<li.+</li.+</ul.+</td.+</table", Pattern.DOTALL)
-                    .matcher(html).find();
-    
-    assertTrue("[" + html + "]", found);
-    
-    
   }
 }
